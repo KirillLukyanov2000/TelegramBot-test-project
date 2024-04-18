8d9815c5ca0ef3694fa0b75dd16ea1520f9ba8d6
Kirill Lukyanov <kirs2000@mail.ru>
2024-04-18 10:36:44 +0300

    Chatbot Game logic updated

---
+++ TelegramBot/src/main/java/ru/lukyanov/telegrambot/Buttons.java
@@ -0,0 +1,15 @@
+package ru.lukyanov.telegrambot;
+
+public class Buttons {
+    public static final String STEP_1_BUTTON = "step_1_btn";
+    public static final String STEP_2_BUTTON = "step_2_btn";
+    public static final String STEP_3_BUTTON = "step_3_btn";
+    public static final String STEP_4_BUTTON = "step_4_btn";
+    public static final String STEP_5_BUTTON = "step_5_btn";
+    public static final String STEP_6_BUTTON = "step_6_btn";
+    public static final String STEP_7_BUTTON = "step_7_btn";
+    public static final String STEP_8_BUTTON = "step_8_btn";
+
+    private Buttons() {
+    }
+}
--- TelegramBot/src/main/java/ru/lukyanov/telegrambot/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/ru/lukyanov/telegrambot/MyFirstTelegramBot.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.util.Map;
 
+import static ru.lukyanov.telegrambot.Buttons.*;
 import static ru.lukyanov.telegrambot.TelegramBotContent.*;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
@@ -22,49 +23,87 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         //test version:
         //super.onUpdateEventReceived(updateEvent);
 
+        if (getMessageText().equals("/glory")) {
+            sendTextMessageAsync("Количество очков славы: " + getUserGlory());
+            if (getUserGlory() < 230) {
+                sendTextMessageAsync("Игра ещё не закончена, продолжай дальше!");
+                sendTextMessageAsync("Пройди игру до конца, снова запроси очки славы и тебя будет ждать сюрприз от меня. Мяу!");
+            } else {
+                sendTextMessageAsync("Ты заработал максимальное количество очков славы! Победа!");
+                sendTextMessageAsync("Порадуй кота, отправь сообщение, содержащее *мяу*, и я тебе добавлю сердечек в моем последнем сообщении!\n");
+            }
+        }
         if (getMessageText().equals("/start") || getMessageText().contains("start") || getMessageText().contains("Start") || getMessageText().equals("Начать") || getMessageText().contains("начать")) {
             setUserGlory(0);
             sendPhotoMessageAsync("step_1_pic");
-            sendTextMessageAsync(STEP_1_TEXT, Map.of("Взлом холодильника", "step_1_btn"));
+            sendTextMessageAsync(STEP_1_TEXT, Map.of("Взлом холодильника", STEP_1_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_1_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_1_BUTTON)) {
             addUserGlory(20);
             sendPhotoMessageAsync("step_2_pic");
-            sendTextMessageAsync(STEP_2_TEXT, Map.of("Взять сосиску! +20 славы", "step_2_btn", "Взять рыбку! +20 славы", "step_2_btn", "Сбросить банку с огурцами! +20 славы", "step_2_btn"));
+            sendTextMessageAsync(STEP_2_TEXT, Map.of("Взять сосиску! +20 славы", STEP_2_BUTTON, "Взять рыбку! +20 славы", STEP_2_BUTTON, "Сбросить банку с огурцами! +20 славы", STEP_2_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_2_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_2_BUTTON)) {
             addUserGlory(20);
             sendPhotoMessageAsync("step_3_pic");
-            sendTextMessageAsync(STEP_3_TEXT, Map.of("Взлом робота пылесоса", "step_3_btn"));
+            sendTextMessageAsync(STEP_3_TEXT, Map.of("Взлом робота пылесоса", STEP_3_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_3_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_3_BUTTON)) {
             addUserGlory(30);
             sendPhotoMessageAsync("step_4_pic");
-            sendTextMessageAsync(STEP_4_TEXT, Map.of("Отправить робопылесос за едой! +30 славы", "step_4_btn", "Проехаться на робопылесосе! +30 славы", "step_4_btn", "Убегать от робопылесоса! +30 славы", "step_4_btn"));
+            sendTextMessageAsync(STEP_4_TEXT, Map.of("Отправить робопылесос за едой! +30 славы", STEP_4_BUTTON, "Проехаться на робопылесосе! +30 славы", STEP_4_BUTTON, "Убегать от робопылесоса! +30 славы", STEP_4_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_4_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_4_BUTTON)) {
             addUserGlory(30);
             sendPhotoMessageAsync("step_5_pic");
-            sendTextMessageAsync(STEP_5_TEXT, Map.of("Надеть и включить GoPro!", "step_5_btn"));
+            sendTextMessageAsync(STEP_5_TEXT, Map.of("Надеть и включить GoPro!", STEP_5_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_5_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_5_BUTTON)) {
             addUserGlory(40);
             sendPhotoMessageAsync("step_6_pic");
-            sendTextMessageAsync(STEP_6_TEXT, Map.of("Бегать по крышам! +40 славы", "step_6_btn", "Нападать на других котов! +40 славы", "step_6_btn", "Нападать на собак из засады! +40 славы", "step_6_btn"));
+            sendTextMessageAsync(STEP_6_TEXT, Map.of("Бегать по крышам! +40 славы", STEP_6_BUTTON, "Нападать на других котов! +40 славы", STEP_6_BUTTON, "Нападать на собак из засады! +40 славы", STEP_6_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_6_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_6_BUTTON)) {
             addUserGlory(40);
             sendPhotoMessageAsync("step_7_pic");
-            sendTextMessageAsync(STEP_7_TEXT, Map.of("Взлом пароля", "step_7_btn"));
+            sendTextMessageAsync(STEP_7_TEXT, Map.of("Взлом пароля", STEP_7_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_7_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_7_BUTTON)) {
             sendPhotoMessageAsync("step_8_pic");
             addUserGlory(50);
-            sendTextMessageAsync(STEP_8_TEXT, Map.of("Выйти во двор", "step_8_btn"));
+            sendTextMessageAsync(STEP_8_TEXT, Map.of("Выйти во двор", STEP_8_BUTTON));
         }
-        if (getCallbackQueryButtonKey().equals("step_8_btn")) {
+        if (getCallbackQueryButtonKey().equals(STEP_8_BUTTON)) {
             sendPhotoMessageAsync("final_pic");
             sendTextMessageAsync(FINAL_TEXT);
+        }
+        if ((getMessageText().contains("мяу") || getMessageText().contains("Мяу")) && getUserGlory() == 230) {
+            var message = getLastSentMessage();
+            editTextMessageAsync(message.getMessageId(), message.getText() +
+                    """
+                                                        
+                            ...........77777777777.......77777777777
+                            ........77777777777777....77777777777777
+                            ......7777777777777777..7777777777777777
+                            ....77777777777777777777777777777777777
+                            ....7777777777777777777777777777777777
+                            ....7777777777777777777777777777777777
+                            ....777777777777777777777777777777777
+                            .....77777777777777777777777777777777
+                            ......777777777777777777777777777777
+                            ........777777777777777777777777777
+                            ..........7777777777777777777777777
+                            ...........77777777777777777777777
+                            ..............7777777777777777777
+                            .................7777777777777777
+                            .....................7777777777
+                            ........................777777
+                            ..........................777
+                            ...........................7""");
+            sendImageMessageAsync("C:\\Users\\tayga\\Downloads\\Template TelegramBot Marathon 1.1\\TelegramBot\\src\\main\\resources\\images\\lovely_cat.jpg");
+        }
+        if ((getMessageText().contains("мяу") || getMessageText().contains("Мяу")) && getUserGlory() < 230) {
+            sendTextMessageAsync("Очков славы недостаточно, чтобы получить подарок от кота");
         }
     }
 
