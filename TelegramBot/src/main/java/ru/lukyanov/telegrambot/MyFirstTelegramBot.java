65e93b1f3a2d2668d1f032086772aef9db4367cb
Kirill Lukyanov <kirs2000@mail.ru>
2024-04-18 09:25:26 +0300

    Chatbot Game logic updated

--- TelegramBot/src/main/java/ru/lukyanov/telegrambot/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/ru/lukyanov/telegrambot/MyFirstTelegramBot.java
@@ -11,7 +11,7 @@ import static ru.lukyanov.telegrambot.TelegramBotContent.*;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     public static final String NAME = "kirs2000_bot"; // bot name
-    public static final String TOKEN = ""; //bot token
+    public static final String TOKEN = "7080101107:AAFfLQe4DkFiZUAzSp0SZ2ejNkj2w20bKtk"; //bot token
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -22,39 +22,48 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         //test version:
         //super.onUpdateEventReceived(updateEvent);
 
-        if (getMessageText().equals("/start")) {
+        if (getMessageText().equals("/start") || getMessageText().contains("start") || getMessageText().contains("Start") || getMessageText().equals("Начать") || getMessageText().contains("начать")) {
             setUserGlory(0);
+            sendPhotoMessageAsync("step_1_pic");
             sendTextMessageAsync(STEP_1_TEXT, Map.of("Взлом холодильника", "step_1_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_1_btn")) {
             addUserGlory(20);
+            sendPhotoMessageAsync("step_2_pic");
             sendTextMessageAsync(STEP_2_TEXT, Map.of("Взять сосиску! +20 славы", "step_2_btn", "Взять рыбку! +20 славы", "step_2_btn", "Сбросить банку с огурцами! +20 славы", "step_2_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_2_btn")) {
             addUserGlory(20);
+            sendPhotoMessageAsync("step_3_pic");
             sendTextMessageAsync(STEP_3_TEXT, Map.of("Взлом робота пылесоса", "step_3_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_3_btn")) {
             addUserGlory(30);
+            sendPhotoMessageAsync("step_4_pic");
             sendTextMessageAsync(STEP_4_TEXT, Map.of("Отправить робопылесос за едой! +30 славы", "step_4_btn", "Проехаться на робопылесосе! +30 славы", "step_4_btn", "Убегать от робопылесоса! +30 славы", "step_4_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_4_btn")) {
             addUserGlory(30);
+            sendPhotoMessageAsync("step_5_pic");
             sendTextMessageAsync(STEP_5_TEXT, Map.of("Надеть и включить GoPro!", "step_5_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_5_btn")) {
             addUserGlory(40);
+            sendPhotoMessageAsync("step_6_pic");
             sendTextMessageAsync(STEP_6_TEXT, Map.of("Бегать по крышам! +40 славы", "step_6_btn", "Нападать на других котов! +40 славы", "step_6_btn", "Нападать на собак из засады! +40 славы", "step_6_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_6_btn")) {
             addUserGlory(40);
+            sendPhotoMessageAsync("step_7_pic");
             sendTextMessageAsync(STEP_7_TEXT, Map.of("Взлом пароля", "step_7_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_7_btn")) {
+            sendPhotoMessageAsync("step_8_pic");
             addUserGlory(50);
             sendTextMessageAsync(STEP_8_TEXT, Map.of("Выйти во двор", "step_8_btn"));
         }
         if (getCallbackQueryButtonKey().equals("step_8_btn")) {
+            sendPhotoMessageAsync("final_pic");
             sendTextMessageAsync(FINAL_TEXT);
         }
     }
